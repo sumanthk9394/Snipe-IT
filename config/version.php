eb7fc7ee4a71c4f70643ccd57ad6691232fdc396
Ravi Kanth <92990044+ravigfx@users.noreply.github.com>
2022-11-09 17:06:58 +0530

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.0.12',
+  'full_app_version' => 'v6.0.12 - build 8876-gbc4c6abe0',
+  'build_version' => '8876',
+  'prerelease_version' => '',
+  'hash_version' => 'gbc4c6abe0',
+  'full_hash' => 'v6.0.12-50-gbc4c6abe0',
+  'branch' => 'master',
+);
